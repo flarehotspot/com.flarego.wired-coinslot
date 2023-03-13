9e740cc2f10d39887da06cbda352dd88568a8004
Adones Pitogo <pitogo.adones@gmail.com>
2023-03-13 22:04:22 +0800

    nothing here

--- app/backend/payment/payment-method.go
+++ app/backend/payment/payment-method.go
@@ -1,9 +1,11 @@
 package payment
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/flarehotspot/sdk/api/payments"
 	"github.com/flarehotspot/sdk/api/utils"
-	"net/http"
 )
 
 type PaymentMethod struct {
@@ -15,7 +17,7 @@ func (p *PaymentMethod) Name() string {
 }
 
 func (p *PaymentMethod) RequestPayment(w http.ResponseWriter, r *http.Request, items []*payments.PurchaseItem, amount *payments.UnitAmount, callbackUrl string) {
-
+  fmt.Fprintf(w, "Please insert coin")
 }
 
 func NewPaymentMethod(utl utils.IUtils) payments.IPaymentMethod {
