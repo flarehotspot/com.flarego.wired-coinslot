9f2f0550b34c1498e286df112737562dd8a7c85a
Adones Pitogo <pitogo.adones@gmail.com>
2023-04-14 16:36:23 +0800

    remove payment admin nav api

--- app/backend/payment/payment-setup.go
+++ app/backend/payment/payment-setup.go
@@ -1,19 +1,11 @@
 package payment
 
 import (
-	"github.com/flarehotspot/sdk/api/payments"
 	"github.com/flarehotspot/sdk/api/plugin"
-	"github.com/flarehotspot/wired-coinslot/app/backend/routes/names"
 )
 
 func PaymentSetup(api plugin.IPluginApi) {
 	paymentApi := api.PaymentsApi()
 	wiredCoinslot := NewPaymentMethod(api.Utils())
-  adminNav := &payments.PaymentAdminNav{
-    Label: "wired_coinslots",
-    Translate: true,
-    RouteName: names.RouteCoinslotsIndex,
-  }
-	paymentApi.AdminSettings(adminNav)
 	paymentApi.NewPaymentsPlugin(wiredCoinslot)
 }
