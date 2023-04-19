8016d21f6af21e6869ae07e66101e98b516a5070
Adones Pitogo <pitogo.adones@gmail.com>
2023-04-20 06:03:58 +0800

    some refactor

--- app/backend/payment/payment-method.go
+++ app/backend/payment/payment-method.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/flarehotspot/sdk/api/currencies"
 	"github.com/flarehotspot/sdk/api/payments"
 	"github.com/flarehotspot/sdk/api/plugin"
-	uuid "github.com/flarehotspot/sdk/libs/go.uuid"
+	// uuid "github.com/flarehotspot/sdk/libs/go.uuid"
 )
 
 type PaymentMethod struct {
@@ -19,28 +18,28 @@ func (self *PaymentMethod) Name() string {
 }
 
 func (self *PaymentMethod) PaymentHandler(w http.ResponseWriter, r *http.Request) {
-	pur, err := self.api.PaymentsApi().ParsePaymentParams(r)
-	if err != nil {
-		fmt.Fprint(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// pur, err := self.api.PaymentsApi().ParsePurchaseRequest(r)
+	// if err != nil {
+		// fmt.Fprint(w, err.Error(), http.StatusInternalServerError)
+		// return
+	// }
 
-	id, err := uuid.NewV4()
-	if err != nil {
-		fmt.Fprint(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	// id, err := uuid.NewV4()
+	// if err != nil {
+		// fmt.Fprint(w, err.Error(), http.StatusInternalServerError)
+		// return
+	// }
 
-	info := payments.PaymentEvent{
-		Event: payments.EventPaymentStarted,
-		Id:    id,
-		Items: pur.Items,
-		Amount: *&payments.UnitAmount{
-			CurrencyCode: currencies.CurrencyPhilippinePeso,
-			Value:        11.1,
-		},
-	}
-	self.api.PaymentsApi().EmitEvent(pur.CallbackUrl, &info)
+	// info := payments.PaymentInfo{
+		// Event: payments.EventStart,
+		// // Id:    id,
+		// // Items: pur.Items,
+		// Amount: *&payments.UnitAmount{
+			// CurrencyCode: currencies.CurrencyPhilippinePeso,
+			// Value:        11.1,
+		// },
+	// }
+	// self.api.PaymentsApi().EmitEvent(pur.CallbackUrl, &info)
 	fmt.Fprintf(w, "Please insert coin")
 }
 
