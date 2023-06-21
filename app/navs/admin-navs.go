0105780c303e28b72475811b3fd09f1b48f83974
Adones Pitogo <pitogo.adones@gmail.com>
2023-06-21 17:18:27 +0800

    refactor navigation api

--- app/navs/admin-navs.go
+++ app/navs/admin-navs.go
@@ -4,14 +4,13 @@ import (
 	"net/http"
 
 	"github.com/flarehotspot/sdk/api/http/navigation"
-	"github.com/flarehotspot/sdk/api/http/navigation/navgen"
 	"github.com/flarehotspot/sdk/api/plugin"
 	"github.com/flarehotspot/wired-coinslot/app/routes/names"
 )
 
 func SetAdminNavs(api plugin.IPluginApi) {
-  adminIndex := navgen.NewAdminNav(api, navigation.CategoryPayments, "coinslots_list", names.RouteCoinslotsIndex)
-  api.NavApi().AdminNavsFn(func(r *http.Request) []navigation.IAdminNavItem {
-    return []navigation.IAdminNavItem{adminIndex}
-  })
+	adminIndex := navigation.NewAdminNav(navigation.CategoryPayments, "coinslots_list", api.HttpApi().Router().UrlForRoute(names.RouteCoinslotsIndex))
+	api.NavApi().AdminNavsFn(func(r *http.Request) []navigation.IAdminNavItem {
+		return []navigation.IAdminNavItem{adminIndex}
+	})
 }
