84c2fb9443023d40ee0395b10cd9533a45b55e8f
Adones Pitogo <pitogo.adones@gmail.com>
2023-08-06 06:43:37 +0800

    using sdk api

--- main.go
+++ main.go
@@ -3,12 +3,21 @@
 package main
 
 import (
-	"github.com/flarehotspot/com.flarego.wired-coinslot/app"
+	"log"
+
+	"github.com/flarehotspot/sdk"
+	"github.com/flarehotspot/sdk/v1.0.0/api"
 	"github.com/flarehotspot/sdk/v1.0.0/api/plugin"
 )
 
 func main() {}
 
-func Init(api plugin.IPluginApi) {
-	app.Init(api)
+func Init(_sdk sdk.SDK) {
+	sym, err := _sdk.GetVersion(api.VERSION)
+	if err != nil {
+		log.Println("Unable to get plugin api: ", err)
+	}
+
+	apiv1 := sym.(plugin.IPluginApi)
+	log.Printf("Success loading plugin: %s", apiv1.Name())
 }
--- main_mono.go
+++ main_mono.go
@@ -3,10 +3,19 @@
 package wiredcoinslot
 
 import (
+	"log"
+
+	"github.com/flarehotspot/sdk"
+	"github.com/flarehotspot/sdk/v1.0.0/api"
 	"github.com/flarehotspot/sdk/v1.0.0/api/plugin"
-	"github.com/flarehotspot/com.flarego.wired-coinslot/app"
 )
 
-func Init(api plugin.IPluginApi) {
-  app.Init(api)
+func Init(_sdk sdk.SDK) {
+	sym, err := _sdk.GetVersion(api.VERSION)
+	if err != nil {
+		log.Println("Unable to get plugin api: ", err)
+	}
+
+	apiv1 := sym.(plugin.IPluginApi)
+	log.Printf("Success loading plugin: %s", apiv1.Name())
 }
