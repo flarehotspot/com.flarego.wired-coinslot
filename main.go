4b02a4764061922511dc609669019703a4b0b550
Adones Pitogo <pitogo.adones@gmail.com>
2023-08-07 06:58:51 +0800

    Revert "using sdk api"
    This reverts commit 84c2fb9443023d40ee0395b10cd9533a45b55e8f.

--- main.go
+++ main.go
@@ -3,21 +3,12 @@
 package main
 
 import (
-	"log"
-
-	"github.com/flarehotspot/sdk"
-	"github.com/flarehotspot/sdk/v1.0.0/api"
+	"github.com/flarehotspot/com.flarego.wired-coinslot/app"
 	"github.com/flarehotspot/sdk/v1.0.0/api/plugin"
 )
 
 func main() {}
 
-func Init(_sdk sdk.SDK) {
-	sym, err := _sdk.GetVersion(api.VERSION)
-	if err != nil {
-		log.Println("Unable to get plugin api: ", err)
-	}
-
-	apiv1 := sym.(plugin.IPluginApi)
-	log.Printf("Success loading plugin: %s", apiv1.Name())
+func Init(api plugin.IPluginApi) {
+	app.Init(api)
 }
