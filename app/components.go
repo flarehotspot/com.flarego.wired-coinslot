1c7d76a384a787da972d6659e3f3ce9132b321a9
Adones Pitogo <pitogo.adones@gmail.com>
2024-05-07 12:13:42 +0800

    remove handler fields

--- app/components.go
+++ app/components.go
@@ -1,19 +1,19 @@
 package app
 
 import (
-	"github.com/flarehotspot/com.flarego.wired-coinslot/app/handlers"
+	// "github.com/flarehotspot/com.flarego.wired-coinslot/app/handlers"
 	"github.com/flarehotspot/com.flarego.wired-coinslot/app/models"
 	"github.com/flarehotspot/sdk/api/http"
 	"github.com/flarehotspot/sdk/api/plugin"
 )
 
 func SetComponents(api sdkplugin.PluginApi, mdl *models.WiredCoinslotModel) {
-	insertCoinHandler := handlers.InsertCoinHandler(api, mdl)
+	// insertCoinHandler := handlers.InsertCoinHandler(api, mdl)
 	api.Http().VueRouter().RegisterPortalRoutes(sdkhttp.VuePortalRoute{
 		RouteName:   "insert-coin",
 		RoutePath:   "/coinslot/:id/insert-coin",
 		Component:   "InsertCoin.vue",
-		HandlerFunc: insertCoinHandler,
+		// HandlerFunc: insertCoinHandler,
 	})
 
 }
