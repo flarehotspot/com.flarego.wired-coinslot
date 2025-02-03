f90d5541301bfd2191ad6b79de8013fd18f4b4a7
Adones Pitogo <pitogo.adones@gmail.com>
2025-02-03 19:46:05 +0800

    TODO: purchase wifi

--- app/navs/admin-navs.go
+++ app/navs/admin-navs.go
@@ -5,11 +5,11 @@ import (
 
 	// "github.com/flarehotspot/com.flarego.wired-coinslot/app/routes/names"
 	// "sdk/api/http/navigation"
-	plugin "sdk/api/plugin"
+	sdkapi "sdk/api"
 	// "sdk/utils/translate"
 )
 
-func SetAdminNavs(api plugin.PluginApi) {
+func SetAdminNavs(api sdkapi.IPluginApi) {
 	// navText := api.Translate(translate.Label, "wired_coinslots")
 	// adminIndex := navigation.NewAdminNav(navigation.CategoryPayments, navText, api.HttpApi().Router().UrlForRoute(names.RouteCoinslotsIndex))
 	// api.NavApi().AdminNavsFn(func(r *http.Request) []navigation.IAdminNavItem {
