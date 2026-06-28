74e8b0206fba0fe98150a655a5e0904d20ddfaf1
Adones Pitogo <adones@flarego.ph>
2026-06-28 10:39:08 +0800

    added icon to coinslot admin nav

--- src/admin-navs.go
+++ src/admin-navs.go
@@ -10,12 +10,13 @@ func SetAdminNavs(api sdkapi.IPluginApi) {
 	api.Http().Navs().AdminNavsFactory(func(r *http.Request) []sdkapi.AdminNavItemOpt {
 		return []sdkapi.AdminNavItemOpt{
 			{
-				Category:  sdkapi.NavCategoryPayments,
-				Label:     api.Translate("label", "Wired Coinslot"),
-				RouteName: "admin:wired-coinslots:index",
-				Keywords:  []string{"wired", "coinslot", "gpio", "payment", "coin", "relay"},
-				Order:     5000,
-			},
+			Category:  sdkapi.NavCategoryPayments,
+			Label:     api.Translate("label", "Wired Coinslot"),
+			RouteName: "admin:wired-coinslots:index",
+			Keywords:  []string{"wired", "coinslot", "gpio", "payment", "coin", "relay"},
+			Order:     5000,
+			Icon:      "<i class='bi bi-coin'></i>",
+		},
 		}
 	})
 }
